cf97cf8698adb92327be517a4a7572d19a3fb6e2
razar2134 <rartarrazar295@gmail.com>
2026-07-14 16:37:50 -0500

    mesa-estudiantil: branding corregido y config final

--- profiledef.sh
+++ profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
+iso_name="mcjdos"
 iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue DVD"
+iso_publisher="MCJdOS Alpha <https://github.com/razar2134/IUR-repo>"
+iso_application="MCJdOS Alpha - Mesa Estudiantil Live/Install"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
